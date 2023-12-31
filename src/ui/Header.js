84df907fa969634e6fe65e8ff07fbf5064baa688
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2023-12-31 19:55:43 +0530

    package commit

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -6,7 +6,7 @@ import UserName from '../features/user/userName'
 export default function Header() {
   return (
     <header className=' flex items-center justify-around bg-orange-300 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
-      <Link to='/' className='tracking-widest text-stone-600'>Pizza-Bay</Link>
+      <Link to='/pizza' className='tracking-widest text-stone-600'>Pizza-Bay</Link>
       <SearchOrder />
       <UserName />
     </header>
